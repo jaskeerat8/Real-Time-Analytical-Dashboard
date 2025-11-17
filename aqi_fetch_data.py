a2358f4b11887239a214909885176328a8758bf9
Aparna Suresh <jaskeerat.nonu@gmail.com>
2025-11-18 04:37:04 +0530

    data fetch

--- aqi_fetch_data.py
+++ aqi_fetch_data.py
@@ -6,7 +6,7 @@ import requests
 import s3fs
 import pandas as pd
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timezone
 import pytz
 from dotenv import load_dotenv
 
@@ -18,9 +18,8 @@ load_dotenv()
 
 # Credentials
 aws_region = "ap-south-1"
-weather_api_key = os.environ.get("weather_api_key")
-aws_access_key_id = os.environ.get("aws_access_key_id")
-aws_secret_access_key = os.environ.get("aws_secret_access_key")
+for key, value in os.environ.items():
+    globals()[key] = value
 
 # s3 Location
 s3_path = "s3://github-projects-resume/Real_Time_Analytical_Dashboard"
@@ -28,8 +27,63 @@ raw_data_path = f"{s3_path}/data/raw"
 final_data_path = f"{s3_path}/data/final"
 pollutant_breakpoints = f"{s3_path}/resources/aqi_concentration_breakpoints.csv"
 
-# Pollutants
-pollutant_avg_period = {"pm2_5": "24", "pm10": "24", "so2": "24", "no2": "24", "co": "8", "o3": "8"}
+
+# Molecular Weights
+molecular_weights = {"pm25": 0, "pm10": 0, "no2": 46.01, "so2": 64.07, "co": 28.01, "o3": 48.00}
+
+def calculate_aqi(df, breakpoint_df):
+    aqi_breakpoint = 0
+    prominent_pollutant = ""
+    for pollutant_key in df.columns:
+        breakpoint_row = breakpoint_df[
+            (breakpoint_df["pollutant"] == pollutant_key) &
+            (breakpoint_df["low_concentration"] <= df[pollutant_key].iloc[0]) &
+            (breakpoint_df["upper_concentration"] >= df[pollutant_key].iloc[0])
+            ].iloc[0]
+
+        c_low = breakpoint_row['low_concentration']
+        c_high = breakpoint_row['upper_concentration']
+        i_low = breakpoint_row['low_aqi']
+        i_high = breakpoint_row['upper_aqi']
+
+        aqi = ((i_high - i_low) / (c_high - c_low)) * (df[pollutant_key].iloc[0] - c_low) + i_low
+        if aqi > aqi_breakpoint:
+            aqi_breakpoint = int(round(aqi))
+            prominent_pollutant = pollutant_key
+    return aqi_breakpoint, prominent_pollutant
+
+
+def get_aqi_in(breakpoint_df):
+    url = f"https://airquality.googleapis.com/v1/currentConditions:lookup?key={google_api_key}"
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "universalAqi": False, "location": {"latitude": lat, "longitude": lon}, "extraComputations": ["POLLUTANT_CONCENTRATION"]
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    aqi_data = response.json()
+
+    current_time = datetime.strptime(aqi_data["dateTime"], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc).astimezone(pytz.timezone("Asia/Kolkata"))
+    pollutant_concentrations = {
+        p["code"]: (
+            p["concentration"]["value"] * (molecular_weights[p["code"]] / 24.45)
+            if p["concentration"]["units"] == "PARTS_PER_BILLION"
+            else p["concentration"]["value"]
+        )
+        for p in aqi_data["pollutants"]
+    }
+
+    pm2_5 = pollutant_concentrations.get("pm25", 0)
+    pm10 = pollutant_concentrations.get("pm10", 0)
+    no2 = pollutant_concentrations.get("no2", 0)
+    so2 = pollutant_concentrations.get("so2", 0)
+    co = pollutant_concentrations.get("co", 0) / 1000
+    o3 = pollutant_concentrations.get("o3", 0)
+
+    df = pd.DataFrame([{"pm2_5": pm2_5, "pm10": pm10, "so2": so2, "co": co, "o3": o3, "no2": no2}])
+    aqi_in, prominent_pollutant = calculate_aqi(df, breakpoint_df)
+    return current_time, aqi_in, pm2_5, pm10, so2, co, o3, no2, prominent_pollutant
+
 
 def get_aqi_us():
     try_count = 0
@@ -53,28 +107,18 @@ def get_aqi_us():
     return 0
 
 
-def get_api_data():
-    url = f"http://api.weatherapi.com/v1/current.json?key={weather_api_key}&q=Chandigarh&aqi=yes"
+def get_weather_data():
+    url = f"http://api.weatherapi.com/v1/current.json?key={weather_api_key}&q=Chandigarh&aqi=no"
     response = requests.get(url, headers={"accept": "application/json"})
     weather_data = response.json()
-    print(weather_data)
 
-    current_time = datetime.fromtimestamp(weather_data["location"]["localtime_epoch"],
-                                          pytz.timezone(weather_data["location"]["tz_id"]))
     temperature = round(weather_data["current"]["temp_c"])
     humidity = round(weather_data["current"]["humidity"])
     uv = round(weather_data["current"]["uv"])
     wind = round(weather_data["current"]["wind_kph"])
     wind_degree = round(weather_data["current"]["wind_degree"])
 
-    pm2_5 = round(weather_data["current"]["air_quality"]["pm2_5"])
-    pm10 = round(weather_data["current"]["air_quality"]["pm10"])
-    so2 = round(weather_data["current"]["air_quality"]["so2"])
-    co = round(weather_data["current"]["air_quality"]["co"] / 1000, 3)
-    o3 = round(weather_data["current"]["air_quality"]["o3"])
-    no2 = round(weather_data["current"]["air_quality"]["no2"])
-
-    return current_time, temperature, humidity, uv, wind, wind_degree, pm2_5, pm10, so2, co, o3, no2
+    return temperature, humidity, uv, wind, wind_degree
 
 
 def raw_to_s3(df):
@@ -82,7 +126,7 @@ def raw_to_s3(df):
                              aws_secret_access_key=aws_secret_access_key)
 
     current_datetime = datetime.now(pytz.timezone("Asia/Kolkata"))
-    file_location = f"{raw_data_path}/date={current_datetime.date()}/output_{current_datetime.strftime("%H_%M_%S")}.snappy.parquet"
+    file_location = f"{raw_data_path}/date={current_datetime.date()}/output_{current_datetime.strftime('%H_%M_%S')}.snappy.parquet"
     bucket_name = file_location.split("/")[2]
     key = "/".join(file_location.split("/")[3:])
 
@@ -101,35 +145,11 @@ def read_s3():
     all_files = fs.glob(f"{raw_data_path}/**/*.parquet")
 
     df = pd.concat([pd.read_parquet(file, filesystem=fs) for file in all_files], ignore_index=True)
-    df = df.drop_duplicates()
-    df.sort_values(["timestamp"], ascending = [True], inplace=True)
-    df.set_index('timestamp', drop=False, inplace=True)
-
-    for pollutant, period in pollutant_avg_period.items():
-        df[pollutant] = df[pollutant].rolling(f'{period}h').mean()
-        df[pollutant] = df[pollutant].round().astype(int)
+    df = df.drop_duplicates(subset=["timestamp", "aqi_in", "pm2_5", "pm10", "so2", "co", "o3", "no2"], keep='last')
+    df["timestamp"] = pd.to_datetime(df["timestamp"], format="%d-%m-%Y %H:%M:%S")
+    df.sort_values(["timestamp"], ascending=True, inplace=True)
     df = df.reset_index(drop=True)
     return df
-
-
-def calculate_aqi(row, breakpoint_df):
-    aqi_in = 0
-    for pollutant_key in pollutant_avg_period.keys():
-        breakpoint_row = breakpoint_df[
-            (breakpoint_df["pollutant"] == pollutant_key) &
-            (breakpoint_df["low_concentration"] <= row[pollutant_key]) &
-            (breakpoint_df["upper_concentration"] >= row[pollutant_key])
-            ].iloc[0]
-
-        c_low = breakpoint_row['low_concentration']
-        c_high = breakpoint_row['upper_concentration']
-        i_low = breakpoint_row['low_aqi']
-        i_high = breakpoint_row['upper_aqi']
-
-        aqi = ((i_high - i_low) / (c_high - c_low)) * (row[pollutant_key] - c_low) + i_low
-        if aqi > aqi_in:
-            aqi_in = int(round(aqi))
-    return aqi_in
 
 
 def final_to_s3(df):
@@ -147,38 +167,59 @@ def final_to_s3(df):
 
 
 def lambda_handler(event=None, context=None):
+    # CPCB Breakpoint Ranges
+    breakpoint_df = pd.read_csv(pollutant_breakpoints)
+
+    # AQI India Standard
+    current_time, aqi_in, pm2_5, pm10, so2, co, o3, no2, prominent_pollutant = get_aqi_in(breakpoint_df)
+
     # AQI US Standard
     aqi_us = get_aqi_us()
 
     # Weather Data API
-    current_time, temperature, humidity, uv, wind, wind_degree, pm2_5, pm10, so2, co, o3, no2 = get_api_data()
+    temperature, humidity, uv, wind, wind_degree = get_weather_data()
 
-    # Write Raw Data
+    # Write Raw Data to s3
     raw_df = pd.DataFrame([{
         "timestamp": current_time,
-        "aqi_us": aqi_us,
-        "temperature": temperature,
-        "humidity": humidity,
-        "uv": uv,
-        "wind": wind,
-        "wind_degree": wind_degree,
+        "aqi_in": aqi_in,
         "pm2_5": pm2_5,
         "pm10": pm10,
         "so2": so2,
         "co": co,
         "o3": o3,
-        "no2": no2
+        "no2": no2,
+        "aqi_us": aqi_us,
+        "prominent_pollutant": prominent_pollutant,
+        "temperature": temperature,
+        "humidity": humidity,
+        "uv": uv,
+        "wind": wind,
+        "wind_degree": wind_degree
     }])
-    print(raw_df)
     raw_to_s3(raw_df)
 
+    # Read all Raw s3 Data
     time.sleep(30)
+    df = read_s3()
+
+    # Calculate AQI 24 hrs
+    df_cpcb = df.copy()
+    df_24hr_avg = df_cpcb.groupby("date", observed=True)[["pm2_5", "pm10", "so2", "no2"]].mean()
+
+    df_cpcb = df_cpcb.set_index("timestamp").sort_index()
+    df_cpcb["co"] = df_cpcb["co"].rolling("8h").mean()
+    df_cpcb["o3"] = df_cpcb["o3"].rolling("8h").mean()
+    df_8hr_max = df_cpcb.resample("D")[["co", "o3"]].max()
+
+    df_24hr_avg.index = pd.to_datetime(df_24hr_avg.index).date
+    df_8hr_max.index = pd.to_datetime(df_8hr_max.index).date
+    final_df = pd.merge(df_24hr_avg, df_8hr_max, left_index=True, right_index=True, how='inner')
+    final_df = final_df.loc[[final_df.index.max()]]
+    aqi_24, prominent= calculate_aqi(final_df, breakpoint_df)
+    df["aqi_24"] = aqi_24
 
     # Write Final Data
-    breakpoint_df = pd.read_csv(pollutant_breakpoints)
-    df = read_s3()
-    df["aqi_in"] = df.apply(lambda row: calculate_aqi(row, breakpoint_df), axis=1)
-    print(df)
     final_to_s3(df)
 
     return {
--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 # Importing Libraries
 import os
 import boto3
-import statistics
 import pickle
 import pandas as pd
 from io import StringIO
@@ -12,22 +11,36 @@ import plotly.express as px
 import dash_mantine_components as dmc
 from dash_iconify import DashIconify
 from dash import Dash, html, dcc, Input, Output
+from flask import Flask
+from flask_caching import Cache
 from dotenv import load_dotenv
+
 
 # Loading Environment
 load_dotenv()
 
 # Credentials
 aws_region = "ap-south-1"
-weather_api_key = os.environ.get("weather_api_key")
-aws_access_key_id = os.environ.get("aws_access_key_id")
-aws_secret_access_key = os.environ.get("aws_secret_access_key")
+for key, value in os.environ.items():
+    globals()[key] = value
+
+# Image Folder
+image_folder = "https://github-projects-resume.s3.ap-south-1.amazonaws.com/Real_Time_Analytical_Dashboard/resources/"
+
+
+# Initialising Dash App
+server = Flask(__name__)
+app = Dash(__name__, server=server, external_stylesheets=["https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"])
+app.title = "AQI Dashboard"
+cache = Cache(app.server, config={"CACHE_TYPE": "SimpleCache",  "CACHE_DEFAULT_TIMEOUT": 300})
+
 
 # Loading ML Model
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
-# Reading Data from Lambda
+# Reading Data from s3
+@cache.memoize()
 def get_data():
     s3_data_path = "s3://github-projects-resume/Real_Time_Analytical_Dashboard/data/final/output.csv"
     bucket_name = s3_data_path.split("/")[2]
@@ -39,28 +52,26 @@ def get_data():
     response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
     csv_content = response['Body'].read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_content))
-    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    df["timestamp"] = pd.to_datetime(df["timestamp"], utc=False)
+    df = df.drop_duplicates("timestamp", keep="first")
     return df
 
+
 # Defining Layout
-app = Dash(__name__, external_stylesheets=["https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"])
-server = app.server
-app.title = "AQI Measure"
 app.layout = dmc.MantineProvider(
     children = html.Div(className="main_layout", children=[
         dcc.Interval(id="time_interval", interval=300000),
-        dcc.Interval(id="model_time_interval", interval=1800000),
         html.Div(className="header", children=[
             dmc.Text("Chandigarh AQI", className="header_text"),
             html.Div(className="header_measure", children=[DashIconify(icon="fluent:temperature-16-filled", color="white", width=25), html.P(id="header_temperature")]),
             html.Div(className="header_measure", children=[DashIconify(icon="carbon:humidity", color="white", width=25), html.P(id="header_humidity")]),
             html.Div(className="header_measure", children=[DashIconify(icon="tabler:uv-index", color="white", width=25), html.P(id="header_uv")]),
             html.Div(className="header_measure", children=[DashIconify(icon="mi:wind", color="white", width=25), html.P(id="header_wind"),
-                html.Img(id="header_wind_direction", src="https://github-projects-resume.s3.ap-south-1.amazonaws.com/Real_Time_Analytical_Dashboard/resources/wind_arrow.png", alt="wind direction", width="20px")]),
+                                                           html.Img(id="header_wind_direction", src=image_folder + "wind_arrow.png", alt="wind direction", width="20px")]),
             dmc.Text(className="header_date", id="header_date")
         ]),
         html.Div(className="aqi_line_chart_container", children=[
-            html.Div(className="aqi_line_chart", children=dcc.Graph(id="aqi_line_chart", config={"displayModeBar": False})),
+            html.Div(className="aqi_line_chart", children=dcc.Graph(id="aqi_line_chart", config={"displayModeBar": False, "responsive": True}, style={"width": "100%", "height": "100%"})),
             html.Div(className="aqi_info", children=[
                 html.Div(className="aqi_reading_container", children=[
                     html.Div(className="aqi_reading", children=[
@@ -88,37 +99,43 @@ app.layout = dmc.MantineProvider(
         html.Div(className="aqi_measure_row", children=[
             html.Div(className="aqi_measure", children=[
                 html.Div(className="aqi_measure_header", children=[DashIconify(icon="streamline:rain-cloud", color="black", width=25), html.P("PM2.5")]),
-                daq.Gauge(className="aqi_measure_gauge", id="PM25_gauge", showCurrentValue=True, value=0, size=170, min=0, max=500, units="ug/m³",
+                html.Div(className="aqi_measure_flag", id="aqi_measure_flag_pm25", children="Dominant Pollutant"),
+                daq.Gauge(className="aqi_measure_gauge", id="pm25_gauge", showCurrentValue=True, digits=0, value=0, min=0, max=500, units="ug/m³",
                     color={"gradient": True, "ranges": {"#377A07": [0, 30], "#9ACD32": [30, 60], "#FFC300":[60, 90], "#F58F09": [90, 120], "#C41206":[120, 250], "#810100": [250, 500]}}
                 )
             ]),
             html.Div(className="aqi_measure", children=[
                 html.Div(className="aqi_measure_header", children=[DashIconify(icon="streamline:rain-cloud-solid", color="black", width=25), html.P("PM10")]),
-                daq.Gauge(className="aqi_measure_gauge", id="PM10_gauge", showCurrentValue=True, value=0, size=170, min=0, max=700, units="ug/m³",
+                html.Div(className="aqi_measure_flag", id="aqi_measure_flag_pm10", children="Dominant Pollutant"),
+                daq.Gauge(className="aqi_measure_gauge", id="pm10_gauge", showCurrentValue=True, digits=0, value=0, min=0, max=700, units="ug/m³",
                     color={"gradient": True, "ranges": {"#377A07": [0, 50], "#9ACD32": [50, 100], "#FFC300":[100, 250], "#F58F09": [250, 350], "#C41206":[350, 430], "#810100": [430, 700]}}
                 )
             ]),
             html.Div(className="aqi_measure", children=[
-                html.Div(className="aqi_measure_header", children=[html.Img(src="https://github-projects-resume.s3.ap-south-1.amazonaws.com/Real_Time_Analytical_Dashboard/resources/Sulfur_Dioxide.png", alt="so2", width="30%"), html.P("Sulfur Dioxide")]),
-                daq.Gauge(className="aqi_measure_gauge", id="so2_gauge", showCurrentValue=True, value=0, size=170, min=0, max=2600, units="ug/m³",
+                html.Div(className="aqi_measure_header", children=[html.Img(src=image_folder + "Sulfur_Dioxide.png", alt="so2", width="30%"), html.P("Sulfur Dioxide")]),
+                html.Div(className="aqi_measure_flag", id="aqi_measure_flag_so2", children="Dominant Pollutant"),
+                daq.Gauge(className="aqi_measure_gauge", id="so2_gauge", showCurrentValue=True, digits=0, value=0, min=0, max=2600, units="ug/m³",
                     color={"gradient": True, "ranges": {"#377A07": [0, 40], "#9ACD32": [40, 80], "#FFC300":[80, 380], "#F58F09": [380, 800], "#C41206":[800, 1600], "#810100": [1600, 2600]}}
                 )
             ]),
             html.Div(className="aqi_measure", children=[
-                html.Div(className="aqi_measure_header", children=[html.Img(src="https://github-projects-resume.s3.ap-south-1.amazonaws.com/Real_Time_Analytical_Dashboard/resources/Carbon_Monoxide.png", alt="co", width="20%"), html.P("Carbon Monoxide")]),
-                daq.Gauge(className="aqi_measure_gauge", id="co_gauge", showCurrentValue=True, value=0, size=170, min=0, max=100, units="mg/m³",
+                html.Div(className="aqi_measure_header", children=[html.Img(src=image_folder + "Carbon_Monoxide.png", alt="co", width="20%"), html.P("Carbon Monoxide")]),
+                html.Div(className="aqi_measure_flag", id="aqi_measure_flag_co", children="Dominant Pollutant"),
+                daq.Gauge(className="aqi_measure_gauge", id="co_gauge", showCurrentValue=True, value=0, min=0, max=100, units="mg/m³",
                     color={"gradient": True, "ranges": {"#377A07": [0, 1], "#9ACD32": [1, 2], "#FFC300":[2, 10], "#F58F09": [10, 17], "#C41206":[17, 34], "#810100": [34, 100]}}
                 )
             ]),
             html.Div(className="aqi_measure", children=[
-                html.Div(className="aqi_measure_header", children=[html.Img(src="https://github-projects-resume.s3.ap-south-1.amazonaws.com/Real_Time_Analytical_Dashboard/resources/Ozone.png", alt="o3", width="15%"), html.P("Ozone")]),
-                daq.Gauge(className="aqi_measure_gauge", id="o3_gauge", showCurrentValue=True, value=0, size=170, min=0, max=1000, units="ug/m³",
+                html.Div(className="aqi_measure_header", children=[html.Img(src=image_folder + "Ozone.png", alt="o3", width="15%"), html.P("Ozone")]),
+                html.Div(className="aqi_measure_flag", id="aqi_measure_flag_o3", children="Dominant Pollutant"),
+                daq.Gauge(className="aqi_measure_gauge", id="o3_gauge", showCurrentValue=True, digits=0, value=0, min=0, max=1000, units="ug/m³",
                     color={"gradient": True, "ranges": {"#377A07": [0, 50], "#9ACD32": [50, 100], "#FFC300":[100, 168], "#F58F09": [168, 208], "#C41206":[208, 748], "#810100": [748, 1000]}}
                 )
             ]),
             html.Div(className="aqi_measure", children=[
-                html.Div(className="aqi_measure_header", children=[html.Img(src="https://github-projects-resume.s3.ap-south-1.amazonaws.com/Real_Time_Analytical_Dashboard/resources/Nitrogen_Dioxide.png", alt="no2", width="20%"), html.P("Nitrogen Dioxide")]),
-                daq.Gauge(className="aqi_measure_gauge", id="no2_gauge", showCurrentValue=True, value=0, size=170, min=0, max=800, units="ug/m³",
+                html.Div(className="aqi_measure_header", children=[html.Img(src=image_folder + "Nitrogen_Dioxide.png", alt="no2", width="20%"), html.P("Nitrogen Dioxide")]),
+                html.Div(className="aqi_measure_flag", id="aqi_measure_flag_no2", children="Dominant Pollutant"),
+                daq.Gauge(className="aqi_measure_gauge", id="no2_gauge", showCurrentValue=True, digits=0, value=0, min=0, max=800, units="ug/m³",
                     color={"gradient": True, "ranges": {"#377A07": [0, 40], "#9ACD32": [40, 80], "#FFC300":[80, 180], "#F58F09": [180, 280], "#C41206":[280, 400], "#810100": [400, 800]}}
                 )
             ])
@@ -137,17 +154,21 @@ app.layout = dmc.MantineProvider(
 )
 def update_aqi_line_chart(time_interval):
     df = get_data()
-    df = df.rename(columns={"aqi_us": "AQI_US", "aqi_in": "AQI_IN"})
+    aqi_chart = px.line(df, x="timestamp", y=["aqi_in", "aqi_us"], template="plotly_white")
 
-    aqi_chart = px.line(df, x="timestamp", y=["AQI_US", "AQI_IN"], template="plotly_white")
-    aqi_chart.update_layout(margin=dict(l=0, r=0, b=0, t=0), height=350)
-    aqi_chart.update_layout(legend=dict(font=dict(color="#000000", size=14, family="Poppins"), traceorder="grouped", orientation="h", x=0.993, y=1, xanchor="right", yanchor="bottom", title_text=""))
+    custom_names = {"aqi_in": "INDIA Standard   ", "aqi_us": "US Standard   "}
+    for trace in aqi_chart.data:
+        trace.name = custom_names.get(trace.name, trace.name)
+
+    aqi_chart.update_layout(autosize=True, margin=dict(l=0, r=25, b=0), height=350)
+    aqi_chart.update_layout(title={"text": "<b>Air Quality Trend</b>", "x": 0.03, "y": 0.92, "yanchor": "top"}, title_font_color="#052F5F", title_font=dict(size=20, family="Poppins"))
+    aqi_chart.update_layout(legend=dict(font=dict(color="#000000", size=14, family="Poppins"), orientation="h", x=0.99, y=1.15, xanchor="right", yanchor="top", title_text=""))
     aqi_chart.update_layout(xaxis_title="", yaxis_title="", legend_title_text="")
     aqi_chart.update_layout(yaxis_showgrid=True, yaxis_ticksuffix="  ", yaxis=dict(dtick=25, tickfont=dict(size=12, family="Poppins", color="black"), griddash="dash", gridwidth=1, gridcolor="#DADADA"))
     aqi_chart.update_layout(xaxis_showgrid=False, xaxis=dict(tickfont=dict(size=12, family="Poppins", color="#000000"), tickangle=0))
     aqi_chart.update_traces(mode="lines+markers", line=dict(width=2), marker=dict(sizemode="diameter", size=8, color="white", line=dict(width=2.5)))
     aqi_chart.update_yaxes(fixedrange=True)
-    aqi_chart.update_xaxes(tickformat="%d %B %Y\n%I:%M%p")
+    aqi_chart.update_xaxes(tickformat="%d %b'%y\n%I:%M%p", tickangle=0, tickmode="auto", automargin=True)
 
     # Hover Label
     aqi_chart.update_layout(hovermode="x unified", hoverlabel=dict(bgcolor="#c1dfff", font_size=12, font_family="Poppins", align="left"))
@@ -156,35 +177,28 @@ def update_aqi_line_chart(time_interval):
 
 
 # Updating AQI Measures
-@app.callback([Output("header_date", "children"),
-     Output("aqi_reading_count_actual", "children"),
-     Output("PM25_gauge", "value"),
-     Output("PM10_gauge", "value"),
-     Output("so2_gauge", "value"),
-     Output("co_gauge", "value"),
-     Output("o3_gauge", "value"),
-     Output("no2_gauge", "value"),
-     Output("header_temperature", "children"),
-     Output("header_humidity", "children"),
-     Output("header_uv", "children"),
-     Output("header_wind", "children"),
+@app.callback(
+    [Output("header_date", "children"), Output("aqi_reading_count_actual", "children"),
+     Output("pm25_gauge", "value"), Output("pm10_gauge", "value"),
+     Output("so2_gauge", "value"), Output("co_gauge", "value"),
+     Output("o3_gauge", "value"), Output("no2_gauge", "value"),
+     Output("header_temperature", "children"), Output("header_humidity", "children"),
+     Output("header_uv", "children"), Output("header_wind", "children"),
     Output("header_wind_direction", "style")],
     Input("time_interval", "n_intervals")
 )
 def update_aqi_measures(time_interval):
     df = get_data()
     measures = df.iloc[-1]
-    aqi_us_count = measures["aqi_us"]
-    aqi_in_count = measures["aqi_in"]
-    aqi = round(statistics.mean([aqi_us_count, aqi_in_count]))
 
+    aqi = measures["aqi_24"]
     time_received = "Last Update (IST):\n" + measures["timestamp"].strftime("%d %B %Y, %I:%M %p")
     temperature = str(measures["temperature"]) + " °C"
     humidity = str(measures["humidity"]) + "%"
     wind = str(measures["wind"]) + " km/hr"
     wind_degree = {
         "transition": "transform 0.5s",
-        "transform": f"rotate({measures["wind_degree"]}deg)"
+        "transform": f"rotate({measures['wind_degree']}deg)"
     }
 
     uv = measures["uv"]
@@ -199,13 +213,36 @@ def update_aqi_measures(time_interval):
     else:
         uv = str(uv) + " (Low)"
 
-    return time_received, aqi, measures["pm2_5"], measures["pm10"], measures["so2"], measures["co"], measures["o3"], measures["no2"], temperature, humidity, uv, wind, wind_degree
+    return time_received, aqi, int(measures["pm2_5"]), int(measures["pm10"]), int(measures["so2"]), measures["co"], int(measures["o3"]), int(measures["no2"]), temperature, humidity, uv, wind, wind_degree
+
+
+# Updating AQI Prominent Pollutant Flag
+@app.callback(
+    [Output("aqi_measure_flag_pm25", "style"), Output("aqi_measure_flag_pm10", "style"),
+    Output("aqi_measure_flag_so2", "style"), Output("aqi_measure_flag_co", "style"),
+    Output("aqi_measure_flag_o3", "style"), Output("aqi_measure_flag_no2", "style")],
+    Input("time_interval", "n_intervals")
+)
+def update_prominent_pollutant_flag(time_interval):
+    df = get_data()
+    prominent_pollutant = df.iloc[-1]["prominent_pollutant"]
+
+    hidden = {"display": "none"}
+    visible = {"display": "block"}
+
+    mapping = {"pm2_5": 0, "pm10": 1, "so2": 2, "co": 3, "o3": 4, "no2": 5}
+
+    styles = [hidden] * 6
+    index = mapping.get(prominent_pollutant)
+    if index is not None:
+        styles[index] = visible
+    return styles
 
 
 # Updating AQI Predicted Value
 @app.callback(
     Output("aqi_reading_count_predicted", "children"),
-    Input("model_time_interval", "n_intervals")
+    Input("time_interval", "n_intervals")
 )
 def update_aqi_predicted_count(time_interval):
     upcoming_hour = (datetime.now().astimezone(pytz.timezone("Asia/Kolkata"))+timedelta(hours=1)).hour
@@ -216,4 +253,4 @@ def update_aqi_predicted_count(time_interval):
 
 # Running Main App
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
